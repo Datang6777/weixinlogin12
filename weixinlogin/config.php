5da27d229489bb906baadf3b1f9328b07c317361
datang <275006777@qq.com>
2017-05-25 15:53:29 +0800

    Update config.php

--- weixinlogin/config.php
+++ weixinlogin/config.php
@@ -1,4 +1,4 @@
 <?php
-define("APPID","wxfeb1976a94110e83");
-define("AppSecret","f1574124e252abc8418c314d4b3994e6");
+define("APPID","wxfebzhao12345671976a94110e83");
+define("AppSecret","zhao1234567f1574124e252abc8418c314d4b3994e6");
 ?>
